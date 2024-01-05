8392049d5f9967ff89e11714bc6ca42fc95aad82
VORNNARO <vornnaro202a@gmail.com>
2024-01-05 14:56:40 +0700

    update new env api

--- src/config/API.js
+++ src/config/API.js
@@ -1,3 +1,3 @@
-export const baseUrl = process.env.REACT_APP_API_URL || "https://api.kshrd-ite.com/api/v1";
-// export const baseUrl = process.env.REACT_APP_API_URL || "http://110.74.194.124:1505/api/v1";
+// export const baseUrl = process.env.REACT_APP_API_URL || "https://api.kshrd-ite.com/api/v1";
+export const baseUrl = process.env.REACT_APP_API_URL || "https://ipa.domrra.com/api/v1";
 export const covidBaseUrl = process.env.REACT_APP_COVID_URL || "https://coronavirus-19-api.herokuapp.com/countries";
